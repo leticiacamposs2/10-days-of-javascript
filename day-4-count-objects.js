deb605e20b8e71066cf213f56fdf515e4642d044
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-01-19 18:03:54 -0200

    incluindo a funcao main para executar a solucao

--- day-4-count-objects.js
+++ day-4-count-objects.js
@@ -35,3 +35,16 @@ function getCount(objects) {
     }
     return res;
 }
+
+function main() {
+    const n = +(readLine());
+    let objects = [];
+    
+    for (let i = 0; i < n; i++) {
+        const [a, b] = readLine().split(' ');
+        
+        objects.push({x: +(a), y: +(b)});
+    }
+    
+    console.log(getCount(objects));
+}
